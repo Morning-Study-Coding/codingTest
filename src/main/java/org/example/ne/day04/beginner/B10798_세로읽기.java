796ceab8e8cc3579a87629f730390b6523d21813
perhona <perhona@kakao.com>
2025-05-02 09:41:43 +0900

    day04 beginner 세로읽기

---
+++ src/main/java/org/example/ne/day04/beginner/B10798_세로읽기.java
@@ -0,0 +1,29 @@
+package org.example.ne.day04.beginner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class B10798_세로읽기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<String> strList = new ArrayList<>();
+        int maxLenght = 0;
+
+        for (int i = 0; i < 5; i++) {
+            String s = br.readLine();
+            strList.add(s);
+            maxLenght = Math.max(maxLenght, s.length());
+        }
+
+        for (int i = 0; i < maxLenght; i++) {
+            for (String str : strList) {
+                if (i < str.length()) {
+                    System.out.print(str.charAt(i));
+                }
+            }
+        }
+    }
+}
