9a13943e317b19935de96c6702da38fa12f9e2ae
perhona <perhona@kakao.com>
2025-05-02 11:37:12 +0900

    틀렸던 부분 수정
    - min 타입 long -> int 변경
    - 정답을 갱신하는 조건(min)을 확인하는 로직과 포인터를 움직이는 로직 분리

--- src/main/java/org/example/ne/day04/middler/B2470_두_용액.java
+++ src/main/java/org/example/ne/day04/middler/B2470_두_용액.java
@@ -6,37 +6,31 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class B2470_두_용액 {
-    /*
-     * 산성용액: 1~10억 양의 정수
-     * 알칼리성 용액: -1~ -10억 음의 정수
-     * 같은 양의 용액 혼합 특성값: 각 합
-     * 0에 가까운 특성값을 가진 용액 만들려고 함
-     * ---
-     * 출력은 오름차순
-     * 두 경우가 있다면 아무거나 1개
-     * */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] result = new int[2];
         int N = Integer.parseInt(br.readLine());
         int[] liquids = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+
         int leftIdx = 0;
         int rightIdx = liquids.length - 1;
-        long min = Long.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
 
-        long prevSum = Math.abs(liquids[leftIdx] + liquids[rightIdx]);
-        while (leftIdx >= 0 && rightIdx < liquids.length && leftIdx < rightIdx) {
-            long sum = Math.abs(liquids[leftIdx] + liquids[rightIdx]);
-            if (sum < min) {
-                min = sum;
-                leftIdx++;
+        while (leftIdx < rightIdx) {
+            int sum = liquids[leftIdx] + liquids[rightIdx];
+            int abs = Math.abs(sum);
+            if (abs < min) {
+                min = abs;
                 result[0] = liquids[leftIdx];
                 result[1] = liquids[rightIdx];
+            }
+
+            if (sum < 0) {
+                leftIdx++;
             } else {
                 rightIdx--;
             }
         }
         System.out.println(result[0] + " " + result[1]);
-
     }
 }
