ab0fa7ec5adbfe88e1c814e133148c8a87d3a51a
Minerva08 <elavl@naver.com>
2025-05-09 01:46:32 +0900

    day7: 비기너 수정

--- src/main/java/org/example/jr/day07/begginer/BJN_31562.java
+++ src/main/java/org/example/jr/day07/begginer/BJN_31562.java
@@ -22,23 +22,32 @@ public class BJN_31562 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Map<String, String> saved = new LinkedHashMap<>();
+        Map<String, String> savedByName = new LinkedHashMap<>();
+        Map<String, String> savedByCode = new LinkedHashMap<>();
 
         int saveSongCnt = Integer.parseInt(st.nextToken());
 
         int tryCnt = Integer.parseInt(st.nextToken());
 
-        Map<String,Integer> cntMap = new LinkedHashMap<>();
 
         for(int i=0; i<saveSongCnt; i++){
             st = new StringTokenizer(br.readLine());
             int songNum = Integer.parseInt(st.nextToken());
             String songName = st.nextToken();
-            String code = st.nextToken().substring(0,2);
 
-            saved.put(songName,code);
+            StringBuilder code = new StringBuilder();
+
+            for(int j=0; j<7; j++){
+
+                code.append(st.nextToken());
+            }
+
+            savedByName.put(songName,String.valueOf(code));
+            savedByCode.put(String.valueOf(code),songName);
 
         }
+
+        List<String> codeList = new ArrayList<>();
 
         for(int i=0; i<tryCnt; i++){
             StringBuilder sb = new StringBuilder();
@@ -47,16 +56,28 @@ public class BJN_31562 {
                 sb.append(st.nextToken());
 
             }
-
-            for(Entry<String, String> s : saved.entrySet()){
-                int sCnt = cntMap.getOrDefault(s.getValue(),0);
-                cntMap.put(s.getKey(),sCnt+1);
-            }
+            codeList.add(String.valueOf(sb));
 
         }
 
-        for(Entry<String, Integer> r : cntMap.entrySet()){
+        for(int i=0; i<codeList.size(); i++){
+            int cnt = 0;
+            String songName = null;
+            for(Entry<String, String> song: savedByName.entrySet()){
+                if(song.getValue().substring(0,3).equals(codeList.get(i))){
+                    cnt++;
+                    songName = savedByCode.get(song.getValue());
+                }
 
+            }
+
+            if(cnt>=2){
+                System.out.println("?");
+            }else if(cnt==1){
+                System.out.println(songName);
+            }else{
+                System.out.println("!");
+            }
 
         }
 
