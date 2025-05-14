34f8da1c8bc42cba78050cc78fbbd571a120f823
perhona <perhona@kakao.com>
2025-05-14 11:55:46 +0900

    체스판 다시 칠하기 완성

--- src/main/java/org/example/ne/day10/middler/B1018_체스판_다시_칠하기.java
+++ src/main/java/org/example/ne/day10/middler/B1018_체스판_다시_칠하기.java
@@ -3,27 +3,26 @@ package org.example.ne.day10.middler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 /*
  * 입력
  *   MXN 크기 보드판
  *
  * 로직
- *   전체 순회하면서 색칠해야 하는 정사각형 찾기 - bfs로 8개까지만 하/좌 방향
+ *   전체 순회하면서 색칠해야 하는 정사각형 찾기
  *   최소로 포함하는 값 찾기
+ *   길찾기나 연결성 문제 아니라서 DFS/BFS 아님
+ *  1. 구간 탐색
+ *      for (int i=0;i<=N;i++) {
+ *          for (int j=0;j<=M-8;j++) {
+ *
+ *  2. 첫 색상 별 색칠 필요 여부 탐색
  *
  * 출력
- * 칠해야하는 정사각형의 최소 개수
+ *  칠해야하는 정사각형의 최소 개수
  *
  * */
 public class B1018_체스판_다시_칠하기 {
-    static boolean[][] BOARD;
-    static boolean[][] VISITED;
-    static int[] dx = new int[]{0, 1};
-    static int[] dy = new int[]{-1, 0};
-    static int RESULT = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,46 +30,37 @@ public class B1018_체스판_다시_칠하기 {
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
 
-        BOARD = new boolean[N][M];
-        String startColor = null;
-        boolean equalStart = true;
+        /* 보드 구성 */
+        String[][] board = new String[N][M];
         for (int i = 0; i < N; i++) {
             String[] data = br.readLine().split("");
-            if (i == 0) startColor = data[0];
             for (int j = 0; j < M; j++) {
-                String square = data[j];
-                if ((!equalStart && startColor.equals(square)) || (equalStart && !startColor.equals(square))) {
-                    BOARD[i][j] = true;
-                }
-                equalStart = !equalStart;
+                board[i][j] = data[j];
             }
-            equalStart = !equalStart;
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (i + 8 <= N && j + 8 <= M) {
-                    VISITED = new boolean[N][M];
-                    VISITED[i][j] = true;
-                    RESULT = 0;
-                    dfs(i, j, 1);
-                }
+        /* 8*8 탐색 - 시작 색상이 W일때, B일때 */
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i <= N - 8; i++) {
+            for (int j = 0; j <= M - 8; j++) {
+                int cnt = Math.min(getCnt(i, j, board, "W"), getCnt(i, j, board, "B"));
+                min = Math.min(min, cnt);
             }
         }
-        System.out.println(RESULT);
+        System.out.println(min);
     }
 
-    private static void dfs(int x, int y, int idx) {
-        if (idx == 9) {
-            return;
-        }
-
-        if (x >= 0 && y >= 0 && !VISITED[x][y]) {
-            VISITED[x][y] = true;
-            RESULT++;
-            for (int i = 0; i < 2; i++) {
-                dfs(x + dx[i], y + dy[i], idx + 1);
+    private static int getCnt(int i, int j, String[][] board, String startColor) {
+        int cnt = 0;
+        for (int k = i; k < i + 8; k++) {
+            for (int l = j; l < j + 8; l++) {
+                if ((k + l) % 2 == 0) {
+                    cnt = board[k][l].equals(startColor) ? cnt : cnt + 1;
+                }else {
+                    cnt = !board[k][l].equals(startColor) ? cnt : cnt + 1;
+                }
             }
         }
+        return cnt;
     }
 }
