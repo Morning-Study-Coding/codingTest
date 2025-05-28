28ab883b06450ec0854cf6e3f614c2a39373d09f
perhona <perhona@kakao.com>
2025-05-28 13:08:30 +0900

    치킨배달 완성

--- src/main/java/org/example/ne/day15/middler/B15686_치킨배달.java
+++ src/main/java/org/example/ne/day15/middler/B15686_치킨배달.java
@@ -3,7 +3,10 @@ package org.example.ne.day15.middler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /*
  * N*N 도시, r행 c열
@@ -13,68 +16,80 @@ import java.util.*;
  * 두 칸 사이 거리는 r1 - r2 + c1 - c2
  * 치킨 거리: 집과 가장 가까운 치킨집과의 거리
  *
- * 치킨 거리가 가장 적게 되는 M개 구하기
+ * 치킨 거리(각 집에서 가장 가까운 치킨집 거리의 합)가 가장 적게 되는 M개 구하기
  *
  * */
 public class B15686_치킨배달 {
-    static int[][] grid;
-    static boolean[][] visited;
-    static int[][] chickenMap;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static class Point {
 
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+
+    static List<Point> houses = new ArrayList<>();
+    static List<Point> chickens = new ArrayList<>();
+    static int[][] chickenMap;
+    static int N, M;
+    static int minDist = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
-        grid = new int[N][N];
-        visited = new boolean[N][N];
-        chickenMap = new int[N][N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
+        // 치킨집, 집 별도 저장 -> 조합 탐색 위함
         for (int i = 0; i < N; i++) {
-            String[] data = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                grid[i][j] = Integer.parseInt(data[j]);
+                String val = st.nextToken();
+                if (val.equals("1")) houses.add(new Point(i, j));
+                if (val.equals("2")) chickens.add(new Point(i, j));
             }
         }
 
-        // 1. 1인 경우 2인 치킨집과의 거리를 모두 찾아서 최소값만 남긴다.
-        // 2. 치킨집이 발견되면 각 집에서 치킨집까지의 거리 저장
-        // 3. 가장 거리 합 적은 치킨집만 M개 남기기?
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (grid[i][j] == 0) visited[i][j] = true;
-                if (grid[i][j] == 1) {
-                    visited[i][j] = true;
-                    dfs(new int[]{i, j}, i, j);
-                }
+        chickenMap = new int[houses.size()][chickens.size()];
+        for (int i = 0; i < houses.size(); i++) {
+            for (int j = 0; j < chickens.size(); j++) {
+                chickenMap[i][j] = Math.abs(houses.get(i).x - chickens.get(j).x) + Math.abs(houses.get(i).y - chickens.get(j).y);
             }
         }
-        List<Integer> result = new ArrayList<>();
 
+        selectChickens(0, new int[M], 0);
+        System.out.println(minDist);
     }
 
-    static void dfs(int[] from, int x, int y) {
-        for (int i = 0; i < dx.length; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-            if (isValidRange(nextX, nextY) && !visited[nextX][nextY]) {
-                visited[nextX][nextY] = true;
-                int value = grid[nextX][nextY];
-                if (value == 2) {
-                    int[] chickenPos = new int[]{nextX, nextY};
-                    int diff = Math.abs(from[0] - nextX) + Math.abs(from[1] - nextY);
-                    chickenMap[nextX][nextY] += diff;
+    static void selectChickens(int startIdx, int[] selectedIdx, int cnt) {
+        if (cnt == M) {
+            System.out.println(Arrays.toString(selectedIdx));
+            int sum = 0;
+            for (int[] h : chickenMap) {
+                int min = Integer.MAX_VALUE;
+                for (int c : selectedIdx) {
+                    min = Math.min(min, h[c]);
                 }
-                dfs(from, nextX, nextY);
-//                visited[nextX][nextY] = false;
+                sum += min;
             }
+            minDist = Math.min(minDist, sum);
+            return;
         }
-    }
 
-    private static boolean isValidRange(int nextX, int nextY) {
-        return nextX >= 0 && nextY >= 0 && nextX < grid.length && nextY < grid.length;
+        for (int i = startIdx; i < chickens.size(); i++) {
+            selectedIdx[cnt] = i;
+            selectChickens(i + 1, selectedIdx, cnt + 1);
+        }
     }
 }
