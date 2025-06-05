8d3890aa6d8f3c9d312477d73bb8ed4636f1092c
perhona <perhona@kakao.com>
2025-06-05 09:56:06 +0900

    주석 추가

--- src/main/java/org/example/ne/day20/middler/B19598_최소_회의실_개수.java
+++ src/main/java/org/example/ne/day20/middler/B19598_최소_회의실_개수.java
@@ -23,7 +23,7 @@ public class B19598_최소_회의실_개수 {
         pq.add(meetings.get(0)[1]); // N >= 1 이므로 첫 회의실 추가
 
         for (int i = 1; i < meetings.size(); i++) {
-            int prevMeeting = pq.poll();
+            int prevMeeting = pq.poll(); // 종료 시간이 가장 빠른 회의
             // 미팅 시작시각이 꺼낸 미팅의 종료시각보다 작으면 회의실 추가
             if (meetings.get(i)[0] < prevMeeting) pq.add(prevMeeting);
             pq.add(meetings.get(i)[1]);
